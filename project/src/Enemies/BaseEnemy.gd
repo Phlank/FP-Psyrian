88ac529a2f66148b791a865bb0e6a97687ed57cb
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2020-11-05 20:05:30 -0500

    gh-7: Created Rotator enemy
    Used assets from Tyrian 2000 to create the art. Also included a base enemy class that features hit points.

---
+++ project/src/Enemies/BaseEnemy.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+export var hitpoints : float
+export var reward : float
+export var contact_damage : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ project/src/Enemies/Rotator.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+export var velocity : float
+export var acceleration : float
+export var damage_contact : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func start_action():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
