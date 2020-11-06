134fa016db6a32ab94037324900210808222acde
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2020-11-06 00:11:58 -0500

    gh-10: Refined movement a little bit

--- project/src/Shared/Player.gd
+++ project/src/Shared/Player.gd
@@ -18,20 +18,22 @@ func _process(delta):
 func _process_movement():
 	if Input.is_action_pressed("move_left"):
 		base_movement.x = clamp(base_movement.x - acceleration, -max_velocity_orthogonal, max_velocity_orthogonal)
-	elif !Input.is_action_pressed("move_right"):
-		base_movement.x = clamp(base_movement.x + deceleration, -max_velocity_orthogonal, 0)
 	if Input.is_action_pressed("move_right"):
 		base_movement.x = clamp(base_movement.x + acceleration, -max_velocity_orthogonal, max_velocity_orthogonal)
-	elif !Input.is_action_pressed("move_left"):
-		base_movement.x = clamp(base_movement.x - deceleration, 0, max_velocity_orthogonal)
+	if not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		if base_movement.x > 0:
+			base_movement.x = clamp(base_movement.x - deceleration, 0, max_velocity_orthogonal)
+		elif base_movement.x < 0:
+			base_movement.x = clamp(base_movement.x + deceleration, -max_velocity_orthogonal, 0)
 	if Input.is_action_pressed("move_up"):
-		base_movement.y = clamp(base_movement.y + acceleration, -max_velocity_orthogonal, max_velocity_orthogonal)
-	elif !Input.is_action_pressed("move_down"):
-		base_movement.y = clamp(base_movement.y - deceleration, 0, max_velocity_orthogonal)
-	if Input.is_action_pressed("move_down"):
 		base_movement.y = clamp(base_movement.y - acceleration, -max_velocity_orthogonal, max_velocity_orthogonal)
-	elif !Input.is_action_pressed("move_up"):
-		base_movement.y = clamp(base_movement.y + deceleration, -max_velocity_orthogonal, 0)
+	if Input.is_action_pressed("move_down"):
+		base_movement.y = clamp(base_movement.y + acceleration, -max_velocity_orthogonal, max_velocity_orthogonal)
+	if not Input.is_action_pressed("move_down") and not Input.is_action_pressed("move_up"):
+		if base_movement.y < 0:
+			base_movement.y = clamp(base_movement.y + deceleration, -max_velocity_orthogonal, 0)
+		elif base_movement.y > 0:
+			base_movement.y = clamp(base_movement.y - deceleration, 0, max_velocity_orthogonal)
 	move_and_slide(base_movement + constant_movement, Vector2(0, 0), false, 10, 0.7, false)
 	_process_sprite_change()
 
