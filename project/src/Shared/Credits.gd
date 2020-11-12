20db7527f09c99f469ce83f265bb50f4d59f9445
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2020-11-12 00:07:41 -0500

    gh-14: Changes made to other files related to credits

--- project/src/Shared/Credits.gd
+++ project/src/Shared/Credits.gd
@@ -1,5 +1,12 @@
 class_name Credits
 extends Node2D
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		$Fade.fade_out()
+
 func _on_RichTextLabel_meta_clicked(meta):
 	OS.shell_open(meta)
+
+func _on_Fade_faded_out():
+	get_tree().change_scene("res://src/Shared/Title.tscn")
